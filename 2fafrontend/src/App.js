261455d818d5a9c9808fcac129bc1365e7b09e6d
BartPelsma <bart.pelsma@hotmail.com>
2023-10-24 09:52:31 +0200

    Startup

---
+++ 2fafrontend/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Routes, Navigate, Outlet } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Pages/Home";
+import Admin from "./Pages/Admin";
+import "./styles.css";
+
+export default function App() {
+
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/admin"
+          element={<Admin/> } />
+      </Routes>
+    </>
+  );
+}
